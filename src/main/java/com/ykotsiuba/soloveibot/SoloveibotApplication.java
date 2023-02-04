777779344ea481f57ecb5c033a58e59ee5d10819
ykotsiuba <jurok3x@gmail.com>
2023-02-04 21:10:17 +0200

    Initial project

---
+++ src/main/java/com/ykotsiuba/soloveibot/SoloveibotApplication.java
@@ -0,0 +1,13 @@
+package com.ykotsiuba.soloveibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloveibotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloveibotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ykotsiuba/soloveibot/SoloveibotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ykotsiuba.soloveibot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloveibotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

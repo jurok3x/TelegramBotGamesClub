01eebecc1855ebc02f03d5a24688099423e3ea2b
ykotsiuba <jurok3x@gmail.com>
2023-02-10 22:06:20 +0200

    Added weather response dto and mapper.

--- src/main/java/com/ykotsiuba/soloveibot/entity/dto/WeatherResponseDto.java
+++ src/main/java/com/ykotsiuba/soloveibot/entity/dto/WeatherResponseDto.java
@@ -1,8 +1,21 @@
 package com.ykotsiuba.soloveibot.entity.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class WeatherResponseDto {
+    
+    private Double temperature;
+    private Double windSpeed;
+    private Integer clouds;
+    private Integer humidity;
+    private Double pressure;
+    private String date;
+    private String sunrise;
+    private String sunset;
+    private String condition;
+    private String icon;
 
 }

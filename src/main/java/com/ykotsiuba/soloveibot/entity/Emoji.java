a5b613199e0a4a6f062abe667c820927be204866
ykotsiuba <jurok3x@gmail.com>
2023-02-10 19:38:55 +0200

    Fixed emojies list of chars.

--- src/main/java/com/ykotsiuba/soloveibot/entity/Emoji.java
+++ src/main/java/com/ykotsiuba/soloveibot/entity/Emoji.java
@@ -2,17 +2,23 @@ package com.ykotsiuba.soloveibot.entity;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RequiredArgsConstructor
 public enum Emoji {
     
-    CLEAR_SKY(0x2600), FEW_CLOUDS(0x1F324), SCATTERED_CLOUDS(0x26C5), BROKEN_CLOUDS(0x2601), SHOWER_RAIN(0x1F327),
-    RAIN(0x1F326), THUNDERSTORM(0x26C8), SNOW(0x2744), MIST(0x1F301), WIND(0x1F4A8), HUMIDITY(0x1F4A7), THERMOMETER(0x1F321),
-    SUNRISE(0x1F305), SUNSET(0x1F307), DEFAULT(0x2753);
+    CLEAR_SKY(Arrays.asList(0x2600)), FEW_CLOUDS(Arrays.asList(0x1F324)), SCATTERED_CLOUDS(Arrays.asList(0x26C5)),
+    BROKEN_CLOUDS(Arrays.asList(0x2601)), SHOWER_RAIN(Arrays.asList(0x1F327)),RAIN(Arrays.asList(0x1F327)), THUNDERSTORM(Arrays.asList(0x26C8)),
+    SNOW(Arrays.asList(0x2744)), MIST(Arrays.asList(0x1F301)), WIND(Arrays.asList(0x1F4A8)), HUMIDITY(Arrays.asList(0x1F4A7)),
+    THERMOMETER(Arrays.asList(0x1F321)), SUNRISE(Arrays.asList(0x1F305)), SUNSET(Arrays.asList(0x1F307)), DEFAULT(Arrays.asList(0x2753));
 
-    private final int symbol;
+    private final List<Integer> symbols;
     
     public String getEmogi() {
-        return new String(Character.toChars(symbol));
+        StringBuilder emoji = new StringBuilder();
+        symbols.forEach(symbol -> emoji.append(Character.toChars(symbol)));
+        return emoji.toString();
     }
 
 }
